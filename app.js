dec41c9018017d0fcf5d2a5efd2dae36696e6273
dhy378jnd <doruleva@gmail.com>
2025-09-19 18:11:04 +0300

    17_18

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// app.js
+//
 document.addEventListener('DOMContentLoaded', () => {
     const noteForm = document.getElementById('note-form');
     const noteTextarea = document.getElementById('note-textarea');
